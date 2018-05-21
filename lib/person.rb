884f1d70f0fa041d000280169d6658a5d48103f7
Josh Cesana <joshuacesana@gmail.com>
2018-05-21 23:47:15 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,10 @@
-class Person 
+class Person
   def initialize(name)
-    @name = name
+    @name = name 
   end 
   
   def name=(new_name)
-    @name = new_name 
+    @name = new_name
   end 
   
   def name 
